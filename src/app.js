166b871adb6520b1a03e2795fd822cf1732e04df
Rostyslav Lytvyn <lytvynrostyk@gmail.com>
2019-08-30 14:56:33 +0300

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import {addExpense} from './actions/expenses';
-import {setTextFilter} from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import {Provider} from "react-redux";
